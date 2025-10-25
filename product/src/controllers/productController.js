4337cbe6e3bf42d76bffdab7c94a896237bba234
lethihien1424 <hle26625@gmail.com>
2025-10-25 08:58:04 +0700

    ct

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -110,14 +110,14 @@ class ProductController {
       res.status(500).json({ message: "Server error" });
     }
   }
-  async getProductById(req, res, next) {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    if (!product) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-    return res.status(200).json(product);
-  }
+  // async getProductById(req, res, next) {
+  //   const { id } = req.params;
+  //   const product = await Product.findById(id);
+  //   if (!product) {
+  //     return res.status(404).json({ message: "Product not found" });
+  //   }
+  //   return res.status(200).json(product);
+  // }
   // async getProductById(req, res, next) {
   //   try {
   //     const token = req.headers.authorization;
